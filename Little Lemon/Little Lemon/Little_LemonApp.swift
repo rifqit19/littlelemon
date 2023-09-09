0559b75d27dd10bfc962d0864fdb54abd549567c
Rifqi Triginandri <rifqit19@gmail.com>
2023-09-10 01:51:00 +0700

    creating project

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by rifqi triginandri on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
